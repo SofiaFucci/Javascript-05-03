7a1667fe51f15185675ff3d65e51ac043dda294f
Sofia Q Fucci <98493542+SofiaFucci@users.noreply.github.com>
2022-03-11 21:07:49 -0300

    Hice un ciclo

---
+++ main.js
@@ -0,0 +1,18 @@
+let contacto = prompt ("Como te querés contactar?")
+
+while (contacto != "otro")
+switch (contacto){
+case "telefono":
+  console.log("Comunicate al 4597-5630")
+break;
+case "email":
+console.log("Escribinos a : contacto@ecoleprevert.org.uk")
+break;
+case "En persona":
+  console.log ("Nuestras oficinas se encuentran en Av corrientes 1179 BA")
+
+
+
+
+
+}
